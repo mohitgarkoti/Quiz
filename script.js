ffad9645cc312c7213aad50eed82ccd032ba26de
mohitgarkoti <107355824+mohitgarkoti@users.noreply.github.com>
2022-09-18 11:50:41 +0530

    new change

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-
 let questionCounter = 0;
 var currentQuestion;
 let availableQuestion = [];
 let availableOption = [];
 
-var questionsNumber, questionText, optionContainer;
+var questionsNumber, questionText, optionContainer, answerIndicaterContainer;
 // Questions will be asked
-const Quiz =
-    [{
-
-        q: 'What is capital of India?',
-        option: ['gandhinagar', 'Surat', 'Delhi', 'mumbai',],
-        answer: 2
+const Quiz = [
+    {
+        q: "What is capital of India?",
+        option: ["gandhinagar", "Surat", "Delhi", "mumbai"],
+        answer: 2,
     },
     {
-        q: 'What is the capital of Thailand?',
-        option: ['Lampang', 'phuket', 'Ayutthaya', 'Bangkok',],
-        answer: 3
+        q: "What is the capital of Thailand?",
+        option: ["Lampang", "phuket", "Ayutthaya", "Bangkok"],
+        answer: 3,
     },
 
     {
-        q: 'What is the capital of Gujarat',
-        option: ['surat', 'vadodara', 'gandhinagar', 'rajkot',],
-        answer: 2
+        q: "What is the capital of Gujarat",
+        option: ["surat", "vadodara", "gandhinagar", "rajkot"],
+        answer: 2,
     },
-    ]
+];
 // [{
 //     id: 0,
 //     q: "What is capital of India?",
@@ -58,20 +56,22 @@ const Quiz =
 
 // ]
 
-console.log("quiz", Quiz)
+console.log("quiz", Quiz);
 
 function setAvailableQuestion() {
     const totalQuestion = Quiz.length;
     console.log(totalQuestion);
     for (let i = 0; i < totalQuestion; i++) {
-        availableQuestion.push(Quiz[i])
+        availableQuestion.push(Quiz[i]);
     }
-    console.log("available question", availableQuestion)
+    console.log("available question", availableQuestion);
 }
 
 function getNewQuestion() {
-    questionsNumber.innerHTML = "Question " + (questionCounter + 1) + "of" + Quiz.length;
-    const questionIndex = availableQuestion[Math.floor(Math.random() * availableQuestion.length)]
+    questionsNumber.innerHTML =
+        "Question " + (questionCounter + 1) + "of" + Quiz.length;
+    const questionIndex =
+        availableQuestion[Math.floor(Math.random() * availableQuestion.length)];
     console.log("question index", questionIndex);
     currentQuestion = questionIndex;
     questionText.innerHTML = currentQuestion.q;
@@ -79,29 +79,29 @@ function getNewQuestion() {
     const index1 = availableQuestion.indexOf(questionIndex);
     availableQuestion.splice(index1, 1);
     // console.log(currentQuestion.option);
-    const optionLen = currentQuestion.option.length
+    const optionLen = currentQuestion.option.length;
     for (let i = 0; i < optionLen; i++) {
-        availableOption.push(i)
-
+        availableOption.push(i);
     }
-    optionContainer.innerHTML = '';
-    console.log(availableOption)
+    optionContainer.innerHTML = "";
+    console.log(availableOption);
     for (let i = 0; i < optionLen; i++) {
-        const optionIndex = availableOption[Math.floor(Math.random() * availableOption.length)];
+        const optionIndex =
+            availableOption[Math.floor(Math.random() * availableOption.length)];
         // console.log(optionIndex)
         const index2 = availableOption.indexOf(optionIndex);
         availableOption.splice(index2, 1);
-        console.log("option", optionIndex)
-        console.log(availableOption)
+        console.log("option", optionIndex);
+        console.log(availableOption);
         const option = document.createElement("div");
         option.innerHTML = currentQuestion.option[optionIndex];
         option.id = optionIndex;
         option.className = "option";
-        optionContainer.appendChild(option)
+        optionContainer.appendChild(option);
         option.setAttribute("onClick", "getResult(this)");
     }
-    console.log(availableOption)
-    //     
+    console.log(availableOption);
+    //
     // }
     questionCounter++;
 }
@@ -109,42 +109,64 @@ function getNewQuestion() {
 function next() {
     if (questionCounter === Quiz.length) {
         console.log("quiz over");
-    }
-    else {
+    } else {
         getNewQuestion();
     }
 }
 
-
-
 function getResult(element) {
-    console.log("element", element)
+    console.log(element)
+    console.log("element", element);
     const id = parseInt(element.id);
+    console.log(id)
     if (id === currentQuestion.answer) {
-        element.classList.add("correct")
-        console.log("correct")
-        // currentQuestion.answer.className("correct")
+        element.classList.add("correct");
+        console.log("correct");
+        updateAnswerIndicator("correct");
+    } else {
+        element.classList.add("incorrect");
+        updateAnswerIndicator("incorrect");
+        const optionLen = optionContainer.children.length;
+        for (let i = 0; i < optionLen; i++) {
+            if (parseInt(optionContainer.children[i].id) === currentQuestion.answer) {
+                optionContainer.children[i].classList.add("correct");
+            }
+        }
     }
-
-    else {
-        element.classList.add("incorrect")
-    }
-    // unclickAble();
+    unclickAble();
 }
 
-// function unclickAble() {
-//     const
-// }
+function unclickAble() {
+    const optionLen = optionContainer.children.length;
+    for (let i = 0; i < optionLen; i++) {
+        optionContainer.children[i].classList.add("already-answerd")
+    }
+}
+
+function answerIndicater() {
+    const totalQuestion = Quiz.length;
+    for (let i = 0; i < totalQuestion; i++) {
+        const indicater = document.createElement("div");
+        answerIndicaterContainer.appendChild(indicater);
+    }
+
+
+}
+function updateAnswerIndicator(markType) {
+    answerIndicaterContainer.children[questionCounter - 1].classList.add(markType)
+}
+
 
 window.onload = function () {
-
-    console.log(Quiz)
+    console.log(Quiz);
 
     questionsNumber = document.querySelector(".question-number");
     questionText = document.querySelector(".question-text");
     optionContainer = document.querySelector(".option-container");
+    answerIndicaterContainer = document.querySelector(".answer-indicater");
 
-    console.log(getNewQuestion)
+    console.log(getNewQuestion);
     setAvailableQuestion();
     getNewQuestion();
-}
+    answerIndicater();
+};
